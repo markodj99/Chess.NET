cdc27dd931fa8f07557703cdd11a3ffbb16a6339
Marko Đurđević <makimarkodjurdjevic@gmail.com>
2023-08-09 22:36:58 +0200

    1800 rejting

--- DiplomskiRad/Helper/Parser.cs
+++ DiplomskiRad/Helper/Parser.cs
@@ -14,7 +14,7 @@ namespace DiplomskiRad.Helper
 
             var currentDirectory = Directory.GetCurrentDirectory();
             var targetFolder = Path.Combine(currentDirectory, "..", "..", "..", "Puzzles");
-            var filepath = Path.Combine(targetFolder, "Puzzles_1750.txt");
+            var filepath = Path.Combine(targetFolder, "Puzzles_1800.txt");
 
             using (var reader = new StreamReader(filepath))
             {
