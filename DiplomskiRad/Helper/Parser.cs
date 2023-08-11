7b300e945665e7598ab4d90cb8c3d69d0985b2f2
Marko Đurđević <makimarkodjurdjevic@gmail.com>
2023-08-11 12:39:16 +0200

    ucitavanje svih puzli

--- DiplomskiRad/Helper/Parser.cs
+++ DiplomskiRad/Helper/Parser.cs
@@ -8,36 +8,55 @@ namespace DiplomskiRad.Helper
 {
     public class Parser
     {
+        private static string[] _folders = { "Rating_250_950", "Rating_1000_2000", "Rating_2050_2800" };
+
         public static List<ChessPuzzle> ParseFile()
         {
             var retVal = new List<ChessPuzzle>();
 
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var targetFolder = Path.Combine(currentDirectory, "..", "..", "..", "Puzzles", "Rating_2050_2800");
-            var filepath = Path.Combine(targetFolder, "Puzzles_2800.txt");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string targetFolder = Path.Combine(currentDirectory, "..", "..", "..", "Puzzles");
 
-            using (var reader = new StreamReader(filepath))
+            foreach (var folder in _folders)
             {
-                while (!reader.EndOfStream)
+                retVal.AddRange(GetPuzzleFromFolder(Path.Combine(targetFolder, folder)));
+            }
+
+            return retVal;
+        }
+
+        private static List<ChessPuzzle> GetPuzzleFromFolder(string folderPath)
+        {
+            var retVal = new List<ChessPuzzle>();
+
+            string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+
+            foreach (var txtFile in txtFiles)
+            {
+                string filePath = Path.Combine(folderPath, txtFile);
+                using (var reader = new StreamReader(filePath))
                 {
-                    var parts = reader.ReadLine().Split("_");
+                    while (!reader.EndOfStream)
+                    {
+                        var parts = reader.ReadLine().Split("_");
 
-                    var wpos = parts[0];
-                    var bPos = parts[1];
-                    var fm = parts[2];
-                    var mo = parts[3];
+                        var wpos = parts[0];
+                        var bPos = parts[1];
+                        var fm = parts[2];
+                        var mo = parts[3];
 
-                    wpos = Regex.Replace(wpos, @"[\[\]\s]", "");
-                    bPos = Regex.Replace(bPos, @"[\[\]\s]", "");
-                    var allWpos = new List<string>(wpos.Split(","));
-                    var allBpos = new List<string>(bPos.Split(","));
+                        wpos = Regex.Replace(wpos, @"[\[\]\s]", "");
+                        bPos = Regex.Replace(bPos, @"[\[\]\s]", "");
+                        var allWpos = new List<string>(wpos.Split(","));
+                        var allBpos = new List<string>(bPos.Split(","));
 
-                    var color = fm[1] == 'W' ? Color.White : Color.Black;
+                        var color = fm[1] == 'W' ? Color.White : Color.Black;
 
-                    mo = Regex.Replace(mo, @"[\[\]\s]", "");
-                    var moveOrder = new List<string>(mo.Split(","));
+                        mo = Regex.Replace(mo, @"[\[\]\s]", "");
+                        var moveOrder = new List<string>(mo.Split(","));
 
-                    retVal.Add(new ChessPuzzle(allWpos, allBpos, color, moveOrder));
+                        retVal.Add(new ChessPuzzle(allWpos, allBpos, color, moveOrder));
+                    }
                 }
             }
 
